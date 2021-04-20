d4cf6c25660e16a82a0d00d106f462b66d6c6ad9
Takashi Furuya <takashi.furuya.0@gmail.com>
2021-04-20 10:37:38 +0900

    バグ修正

--- src/components/OpportunityDetail.vue
+++ src/components/OpportunityDetail.vue
@@ -296,7 +296,7 @@ export default class OpportunityDetail extends Vue {
     this.sum_working_time = 0;
     this.axios({
       method: "get",
-      url: `${this.base_url}/drm/lancers/opportunitywork/?opportunity_id=${this.$route.params.id}&limit=1000`,
+      url: `${this.base_url}/drm/lancers/opportunitywork/?opportunity=${this.$route.params.id}&limit=1000`,
       headers: {
         'Content-Type': 'application/json'
       }
